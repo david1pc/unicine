f3bcc4b0dd18844ca6efa83b3838b202ac90e86e
David <davidalejandrorh@hotmail.com>
2022-09-11 17:47:21 -0500

    Primer commit

---
+++ negocio/src/main/java/co/edu/uniquindio/unicine/NegocioApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NegocioApplication.class);
+    }
+}
